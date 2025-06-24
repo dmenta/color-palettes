4df3a645a085cd407226c1db5e0219b7e00b3cc7
Diego M. <diegomenta@gmail.com>
2025-06-24 07:00:27 -0300

    Ajuste de valores de pasos más notorios

--- src/app/color/model/color-models-definitions.ts
+++ src/app/color/model/color-models-definitions.ts
@@ -2,19 +2,19 @@ import { ColorComponent, ColorModel, ColorModelName } from "./colors.model";
 
 export const colorModels: ColorModel[] = [
   new ColorModel("rgb", [
-    new ColorComponent("Red", 255, "", 0, 32),
-    new ColorComponent("Green", 255, "", 0, 32),
-    new ColorComponent("Blue", 255, "", 0, 32),
+    new ColorComponent("Red", 255, "", 0, 20),
+    new ColorComponent("Green", 255, "", 0, 20),
+    new ColorComponent("Blue", 255, "", 0, 20),
   ]),
   new ColorModel("hsl", [
-    new ColorComponent("Hue", 360, "deg", 0, 75),
-    new ColorComponent("Saturation", 100, "%", 0, 25),
-    new ColorComponent("Lightness", 100, "%", 0, 50),
+    new ColorComponent("Hue", 360, "deg", 0, 55),
+    new ColorComponent("Saturation", 100, "%", 0, 20),
+    new ColorComponent("Lightness", 100, "%", 0, 35),
   ]),
   new ColorModel("oklch", [
-    new ColorComponent("Lightness", 100, "%", 2, 50),
-    new ColorComponent("Chroma", 0.3, "", 3, 30),
-    new ColorComponent("Hue", 360, "", 2, 75),
+    new ColorComponent("Lightness", 100, "%", 2, 35),
+    new ColorComponent("Chroma", 0.3, "", 3, 20),
+    new ColorComponent("Hue", 360, "", 2, 55),
   ]),
 ];
 
