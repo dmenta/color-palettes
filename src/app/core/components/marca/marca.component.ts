661423b7e02733e2c585c4f0e03f32a466572755
Diego M. <diegomenta@gmail.com>
2025-06-30 19:32:27 -0300

    Nueva marca y mejoras en el header

---
+++ src/app/core/components/marca/marca.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "zz-marca",
+  imports: [],
+  templateUrl: "./marca.component.html",
+})
+export class MarcaComponent {}
--- src/app/features/palettes/header/palettes-header.component.ts
+++ src/app/features/palettes/header/palettes-header.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { DarkModeToggleComponent } from "../../../core/components/dark-mode-toggle/dark-mode-toggle.component";
 import { PaletteSelectorComponent } from "../../../color/components/palette-selector/palette-selector.component";
+import { MarcaComponent } from "../../../core/components/marca/marca.component";
 
 @Component({
   selector: "zz-palettes-header",
-  imports: [DarkModeToggleComponent, PaletteSelectorComponent],
+  imports: [DarkModeToggleComponent, PaletteSelectorComponent, MarcaComponent],
   templateUrl: "./palettes-header.component.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
