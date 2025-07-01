8ebda7caf8906b12982b2c68e3574c0464ebeb6f
Diego M. <diegomenta@gmail.com>
2025-06-30 23:24:29 -0300

    Mejoras en la marca Agregado formato de marca

--- src/app/core/components/marca/marca.component.ts
+++ src/app/core/components/marca/marca.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, input } from "@angular/core";
 
 @Component({
   selector: "zz-marca",
   imports: [],
   templateUrl: "./marca.component.html",
 })
-export class MarcaComponent {}
+export class MarcaComponent {
+  format = input("horizontal" as "horizontal" | "half" | "quarter" | "vertical");
+}
