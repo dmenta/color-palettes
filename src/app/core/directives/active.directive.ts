ed7d685b411ee496c83b84c91cb2b5982b5322a4
Diego M. <diegomenta@gmail.com>
2025-07-03 00:58:26 -0300

    En desuso

--- src/app/core/directives/active.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive } from "@angular/core";
-
-@Directive({
-  selector: "[zz-active], zzActive",
-  host: {
-    class: `active:not-disabled:scale-90`,
-  },
-})
-export class ActiveDirective {}
