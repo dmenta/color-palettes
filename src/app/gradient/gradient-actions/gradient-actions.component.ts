d6a3e8629c9dbf952637a0b62798c6891b756d6d
Diego M. <diegomenta@gmail.com>
2025-07-05 11:19:49 -0300

    Gradient orientation in SVG

--- src/app/gradient/gradient-actions/gradient-actions.component.ts
+++ src/app/gradient/gradient-actions/gradient-actions.component.ts
@@ -49,6 +49,27 @@ export class GradientActionsComponent {
   }
 
   toSVG() {
+    const orientation = this.state.orientation();
+    let x1 = 0;
+    let x2 = 0;
+    let y1 = 0;
+    let y2 = 0;
+
+    if (orientation.includes("right")) {
+      x2 = 400;
+    }
+    if (orientation.includes("left")) {
+      x1 = 400;
+    }
+
+    if (orientation.includes("bottom")) {
+      y2 = 200;
+    }
+
+    if (orientation.includes("top")) {
+      y1 = 200;
+    }
+
     const pasos = this.state
       .gradient()
       .pasosRGB.map(
@@ -61,7 +82,7 @@ export class GradientActionsComponent {
 <svg width="100%" height="100%" viewBox="0 0 400 200" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
     <rect x="0" y="0" width="400" height="200" style="fill:url(#_Linear1);"/>
     <defs>
-        <linearGradient id="_Linear1" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(400,200,-200,400,0,0)">
+        <linearGradient id="_Linear1" x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" gradientUnits="userSpaceOnUse" >
 ${pasos}
         </linearGradient>
     </defs>
