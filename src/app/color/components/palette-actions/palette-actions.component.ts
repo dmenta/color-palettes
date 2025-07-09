b1c124e4727734d178948e151c6cf757397b1c4b
Diego M. <diegomenta@gmail.com>
2025-07-09 19:45:59 -0300

    Save palette as SVG

--- src/app/color/components/palette-actions/palette-actions.component.ts
+++ src/app/color/components/palette-actions/palette-actions.component.ts
@@ -11,6 +11,8 @@ import { rgbToHex } from "../../model/color";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaletteActionsComponent {
+  private readonly baseDate = new Date(2025, 0, 1).valueOf();
+
   store = inject(PaletteStoreService);
   state = inject(ColorStateService);
   copyService = inject(CopyService);
@@ -56,6 +58,10 @@ export class PaletteActionsComponent {
     this.copyService.copy(valores, "Palette colors copied!");
   }
 
+  saveSVG() {
+    this.paletteToSVG(this.state.palette().swatches.map((s) => s.rgb));
+  }
+
   private paletteValues(palette: Palette) {
     const rgbParts = [] as string[];
     const hexParts = [] as string[];
@@ -75,5 +81,22 @@ export class PaletteActionsComponent {
 
       return `${colors}\r\n\r\n${rgbs}\r\n\r\n${hexs}`;
     }
+  }
+
+  private paletteToSVG(rgbColors: string[], size: number = 40) {
+    const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="${rgbColors.length * size}" height="50">
+      <rect width="100%" height="100%" fill="white"/>
+      ${rgbColors
+        .map((color, index) => `<rect x="${index * size}" y="0" width="${size}" height="${size}" fill="${color}"/>`)
+        .join("\n")}
+    </svg>`;
+
+    const blob = new Blob([svgContent], { type: "image/svg+xml" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `colorina-palette-${new Date().valueOf() - this.baseDate}.svg`;
+    a.click();
+    URL.revokeObjectURL(url);
   }
 }
