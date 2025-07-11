59485f36fb4e4437174b671df8ffd8a1179e2b67
Diego M. <diegomenta@gmail.com>
2025-07-11 17:57:03 -0300

    Arreglada exportaciíon svg

--- src/app/gradient/models/gradient-stops.ts
+++ src/app/gradient/models/gradient-stops.ts
@@ -151,13 +151,9 @@ function toSVG(stops: GradientStop[], angleDegrees: number = 0) {
   const width = 1920;
   const height = 1080;
 
-  const maxSize = Math.max(width, height);
-  const minSize = Math.min(width, height);
-  const fullHypotenusa = Math.sqrt(width * width + height * height);
-  let { x1: _x1, y1: _y1, x2: _x2, y2: _y2, hypotenusa } = gradientOrientation(width, height, angleDegrees);
-  hypotenusa = Math.abs(hypotenusa);
-  const origenX = (maxSize - hypotenusa) / 2;
-  const origenY = (minSize - fullHypotenusa) / 2;
+  const { xGrad, yGrad, gradWidth, gradHeight } = gradientOrientation2d(width, height, angleDegrees - 90);
+  console.log("xGrad", xGrad, "yGrad", yGrad);
+  console.log("xGrad", gradHeight, "yGrad", gradWidth);
 
   const pasos = stops
     .map((p) => `        <stop offset="${(p.offset / 100).toFixed(3)}" style="stop-color:${p.color};stop-opacity:1"/>`)
@@ -169,17 +165,55 @@ function toSVG(stops: GradientStop[], angleDegrees: number = 0) {
      xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/"
       style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
 
-    <rect x="${origenX}" y="${origenY}" width="${hypotenusa}" height="${fullHypotenusa}"  style=" transform-origin: center center; transform: rotate(${
-    angleDegrees - 90
-  }deg); fill:url(#_Linear1);"/>
+    <rect x="${(width - gradHeight) / 2}" y="${
+    (height - gradWidth) / 2
+  }" width="${gradHeight}" height="${gradWidth}"  style="fill:url(#_Linear1);transform-origin: center center; transform: rotate(${
+    angleDegrees - 180
+  }deg);"/>
     <rect x="0" y="0" width="${width}" height="${height}" stroke="black" fill="none" />
 
   <defs>
-        <linearGradient id="_Linear1" x1="${origenX}" y1="0" x2="${hypotenusa}" y2="0" gradientUnits="userSpaceOnUse" >
+        <linearGradient id="_Linear1" x1="0" y1="${(height - gradWidth) / 2}" x2="0" y2="${
+    height - (height - gradWidth) / 2
+  }" gradientUnits="userSpaceOnUse" >
 ${pasos}
         </linearGradient>
     </defs>
 </svg>`;
+}
+
+function gradientOrientation2d(width: number, height: number, angleDegrees: number) {
+  const halfHypotenusa = Math.sqrt(((width / 2) * width) / 2 + ((height / 2) * height) / 2);
+  const hypotenusaAngle = Math.atan2(height / 2, width / 2);
+
+  angleDegrees = Math.abs(angleDegrees % 360);
+  console.log("angleDegrees", angleDegrees);
+
+  const currRad = (angleDegrees * Math.PI * 2) / 360;
+  const angle = currRad + hypotenusaAngle;
+  const flipAngle = currRad - hypotenusaAngle;
+  const x = Math.abs(Math.cos(angle) * halfHypotenusa);
+  const y = Math.abs(Math.sin(angle) * halfHypotenusa);
+  const flipX = Math.abs(Math.cos(flipAngle) * halfHypotenusa);
+  const flipY = Math.abs(Math.sin(flipAngle) * halfHypotenusa);
+
+  const gradWidth = Math.max(x, flipX) * 2;
+  const gradHeight = Math.max(y, flipY) * 2;
+  const gradX = (width - gradWidth) / 2;
+  const gradY = (height - gradHeight) / 2;
+
+  const newWidth = gradWidth / 2;
+  const newCoseno = Math.cos(currRad) * newWidth;
+  const newSeno = Math.sin(currRad) * newWidth;
+
+  return {
+    x: gradX,
+    y: gradY,
+    gradWidth,
+    gradHeight,
+    xGrad: Math.round(Math.abs(newCoseno)),
+    yGrad: Math.round(Math.abs(newSeno)),
+  };
 }
 
 function gradientOrientation(width: number, height: number, angleDegrees: number) {
